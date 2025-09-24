90ac27f65e1047aaba7a7c30e988f8f9d4d89e48
Samuel <samuel.sneves@hotmail.com>
2025-09-24 14:07:42 -0300

    Transição para o PHP

---
+++ paginas/home.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<title>Mídia Ambiental</title>
+		<link rel="icon" type="image/x-icon" href="./../imgs/site-icon.png">
+	</head>	
+	<body>
+		<?php include_once('./menu-superior.php'); ?>
+	
+		<h1>Mídia Ambiental</h1>
+	</body>
+</html>
+<?php
+
+
+
+?>
+
+
+
+
---
+++ paginas/menu-superior.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="./../estilos/padrao.css">
+		<link rel="stylesheet" href="./../estilos/menu-superior.css">
+	</head>
+	<body>
+		<section id="menu-superior">
+			<p>Impactos Impactos</p>
+		</section>
+	</body>
+</html>
+
+
+
+
